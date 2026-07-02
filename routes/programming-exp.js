448a7a8fc2eef2e815037e6a4f31db6a02000584
U15 Maizuru <u15procon.maizuru@gmail.com>
2026-07-02 23:04:33 +0900

    refactor: programming.jsとprogramming-exp.jsの重複コードを共通ファクトリ関数に統合

--- routes/programming-exp.js
+++ routes/programming-exp.js
@@ -1,20 +1,3 @@
-var express = require('express');
-var router = express.Router();
-const logger = require('../bin/logger.js');
+var createProgrammingRouter = require('./programming').createProgrammingRouter;
 
-var fs = require('fs');
-var path = require('path');
-
-//GET page
-router.get('/', function (req, res, next) {
-  try {
-    var lg = req.cookies.lng;
-    var LNG = JSON.parse(fs.readFileSync(path.join(__dirname, '..', "language", lg, "programming.json"), "utf-8"));
-  }
-  catch (e) {
-    var LNG = JSON.parse(fs.readFileSync(path.join(__dirname, '..', "language", "ja", "programming.json"), "utf-8"));
-  }
-  res.render('programming-exp', { "title": 'プログラミング', "LNG": LNG });
-});
-
-module.exports = router;
+module.exports = createProgrammingRouter('programming-exp');
--- routes/programming.js
+++ routes/programming.js
@@ -1,20 +1,24 @@
 var express = require('express');
-var router = express.Router();
 const logger = require('../bin/logger.js');
 
 var fs = require('fs');
 var path = require('path');
 
-//GET page
-router.get('/', function (req, res, next) {
-  try {
-    var lg = req.cookies.lng;
-    var LNG = JSON.parse(fs.readFileSync(path.join(__dirname, '..', "language", lg, "programming.json"), "utf-8"));
-  }
-  catch (e) {
-    var LNG = JSON.parse(fs.readFileSync(path.join(__dirname, '..', "language", "ja", "programming.json"), "utf-8"));
-  }
-  res.render('programming', { "title": 'プログラミング', "LNG": LNG });
-});
+// programming-exp.js からも再利用する（画面名以外は完全に同一処理のため）
+function createProgrammingRouter(viewName) {
+  var router = express.Router();
+  router.get('/', function (req, res, next) {
+    try {
+      var lg = req.cookies.lng;
+      var LNG = JSON.parse(fs.readFileSync(path.join(__dirname, '..', "language", lg, "programming.json"), "utf-8"));
+    }
+    catch (e) {
+      var LNG = JSON.parse(fs.readFileSync(path.join(__dirname, '..', "language", "ja", "programming.json"), "utf-8"));
+    }
+    res.render(viewName, { "title": 'プログラミング', "LNG": LNG });
+  });
+  return router;
+}
 
-module.exports = router;
+module.exports = createProgrammingRouter('programming');
+module.exports.createProgrammingRouter = createProgrammingRouter;
