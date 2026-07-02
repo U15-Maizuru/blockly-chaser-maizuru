6d81354b1dbe62191e277df48aeea827d48c215f
U15 Maizuru <u15procon.maizuru@gmail.com>
2026-07-02 23:35:53 +0900

    refactor: programming.jsのi18n読み込みをtool/language_load.jsに統一
    programming.jsだけi18n共通化(tool/language_load.js)の対象から漏れており、fs.readFileSync+try/catchで独自にja/ja-k読み込みを再実装していた。他6ルートと同じloadLangJson/pickByCookieパターンに揃える。

--- routes/programming.js
+++ routes/programming.js
@@ -1,21 +1,15 @@
 var express = require('express');
 const logger = require('../bin/logger.js');
 
-var fs = require('fs');
-var path = require('path');
+var languageLoad = require('../tool/language_load.js');
+
+var LNG = languageLoad.loadLangJson('programming.json');
 
 // programming-exp.js からも再利用する（画面名以外は完全に同一処理のため）
 function createProgrammingRouter(viewName) {
   var router = express.Router();
   router.get('/', function (req, res, next) {
-    try {
-      var lg = req.cookies.lng;
-      var LNG = JSON.parse(fs.readFileSync(path.join(__dirname, '..', "language", lg, "programming.json"), "utf-8"));
-    }
-    catch (e) {
-      var LNG = JSON.parse(fs.readFileSync(path.join(__dirname, '..', "language", "ja", "programming.json"), "utf-8"));
-    }
-    res.render(viewName, { "title": 'プログラミング', "LNG": LNG });
+    res.render(viewName, { "title": 'プログラミング', "LNG": languageLoad.pickByCookie(req, LNG) });
   });
   return router;
 }
